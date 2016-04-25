9c9690fe7fb51f5909da3b6d378827e6604fbe12
jakesnyder-hansen <j.snyderhansen.321@gmail.com>
2016-04-25 18:44:07 -0500

    Major Update

--- src/Train.java
+++ src/Train.java
@@ -2,5 +2,21 @@
  * Created by jakesnyder-hansen on 4/25/16.
  */
 public class Train {
-    public Train()
+
+    public Train(int n, int m){
+        startLocation = m;
+
+        numTrainCars = new TrainCar[n];
+        for (int i = 0; i < numTrainCars.length; i++){
+            numTrainCars[i] = new TrainCar();
+        }
+
+    }
+
+    public Passenger addPassenger(Passenger n){
+
+    }
+
+    private int startLocation;
+    private TrainCar[] numTrainCars;
 }
--- src/TrainCar.java
+++ src/TrainCar.java
@@ -4,6 +4,34 @@
 public class TrainCar {
 
     public TrainCar(){
-
+        trainlength = new Q1[23];
+        for (int i = 0; i <trainlength.length; i++){
+            trainlength[i] = new Q1();
+        }
+        count = 0;
     }
+
+    public void addPassenger(Passenger n){
+        if (count < 50) {
+            int stop = n.getDestinationstop();
+            trainlength[stop - 1].add(n);
+            count++;
+        }
+    }
+
+    public Passenger removePassenger(Stop n){
+        //Note will have to iterate all the way through
+        int stopNumber = n.getStopNumber();
+        return (Passenger) trainlength[stopNumber - 1].remove();
+    }
+
+    public boolean isFull(){
+        if (count < 50){
+            return false;
+        }
+        else return true;
+    }
+
+    private int count;
+    private Q1[] trainlength;
 }
