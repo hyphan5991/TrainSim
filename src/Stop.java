2de40dfba84f8bc7453dc71f1bd2d5e0c0357a12
jakesnyder-hansen <j.snyderhansen.321@gmail.com>
2016-04-25 19:00:50 -0500

    Major/minor Update

--- src/Stop.java
+++ src/Stop.java
@@ -3,9 +3,25 @@
  */
 public class Stop {
 
+    public Stop(int n){
+        stopNumber = n;
+        east = new Q1();
+        west = new Q1();
+    }
+
+    public void addPassenger(Passenger n){
+        if (n.getDirectionvector() < 0){
+            west.add(n);
+        }
+        else east.add(n);
+    }
+
+
     public int getStopNumber() {
         return stopNumber;
     }
 
     private int stopNumber;
+    private Q1 east;
+    private Q1 west;
 }
