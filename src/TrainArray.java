d306377af2b8b63c13b637413a19783263c8bd30
jakesnyder-hansen <j.snyderhansen.321@gmail.com>
2016-04-25 19:51:20 -0500

    minor TrainArray update

--- src/TrainArray.java
+++ src/TrainArray.java
@@ -18,18 +18,20 @@ public class TrainArray {
         else trainList[start - 1][0] = n;
     }
 
-    public void move(Train n){
+    public boolean move(Train n){
         int current = n.getCurrentLocation() - 1;
         int vector = n.getDirectionVector();
 
         if (current == 0 && vector < 0){
             if (trainList[22][1] != null){
                 trainList[22][1] = n;
+                return true;
             }
         }
         else if (current == 22 && vector > 0){
             if (trainList[0][0] != null){
                 trainList[0][0] = n;
+                return true;
             }
         }
         else{
@@ -37,15 +39,18 @@ public class TrainArray {
                 if (trainList[current - 1][1] != null){
                     trainList[current - 1][1] = n;
                     n.setCurrentLocation(current - 1);
+                    return true;
                 }
             }
             else {
                 if (trainList[current + 1][0] != null){
                     trainList[current + 1][0] = n;
                     n.setCurrentLocation(current + 1);
+                    return true;
                 }
             }
         }
+        return false;
 
     }
 }
