3dbb8eccf35df23d4d11614f48dae2cffa7e02b7
hyphan5991 <j.snyderhansen.321@gmail.com>
2016-04-26 22:09:29 -0500

    minor changes

--- src/GreenlineSim.java
+++ src/GreenlineSim.java
@@ -8,6 +8,7 @@ import java.io.PrintWriter;
 public class GreenlineSim {
     static PQ agenda = new PQ();
     public static Q1 passengerArray = new Q1();
+    public static Q1 lineLengthData = new Q1();
     public static int numTrainsR = 23;
     public static int numCarsR = 3;
     public static int rushHourMultiplier;
@@ -29,132 +30,6 @@ public class GreenlineSim {
             new TrainEvent(listofTrains[20]), new TrainEvent(listofTrains[21]), new TrainEvent(listofTrains[22])};
 
 
-//    public static void setup(int numTrains, int numCars){
-//
-//        Train firstTrain = new Train(numCars, 11, 1);
-//        trainQ.add(firstTrain);
-//        TrainEvent firstTrainEvent = new TrainEvent(firstTrain);
-//        eventQ.add(firstTrainEvent);
-//
-//        Train secondTrain = new Train(numCars, 11, -1);
-//        trainQ.add(secondTrain);
-//        TrainEvent secondTrainEvent = new TrainEvent(secondTrain);
-//        eventQ.add(secondTrainEvent);
-//
-//        Train thirdTrain = new Train(numCars, 1, 1);
-//        trainQ.add(thirdTrain);
-//        TrainEvent thirdTrainEvent = new TrainEvent(thirdTrain);
-//        eventQ.add(thirdTrainEvent);
-//
-//
-//        Train fourthTrain = new Train(numCars, 1, -1);
-//        trainQ.add(fourthTrain);
-//        TrainEvent fourthTrainEvent = new TrainEvent(fourthTrain);
-//        eventQ.add(fourthTrainEvent);
-//
-//        Train fifthTrain = new Train(numCars, 23, 1);
-//        trainQ.add(fifthTrain);
-//        TrainEvent fifthTrainEvent = new TrainEvent(fifthTrain);
-//        eventQ.add(fifthTrainEvent);
-//
-//        Train sixthTrain = new Train(numCars, 23, -1);
-//        trainQ.add(sixthTrain);
-//        TrainEvent sixthTrainEvent = new TrainEvent(sixthTrain);
-//        eventQ.add(sixthTrainEvent);
-//
-//        Train seventhTrain = ;
-//        trainQ.add(seventhTrain);
-//        TrainEvent seventhTrainEvent = new TrainEvent(seventhTrain);
-//        eventQ.add(seventhTrainEvent);
-//
-//        Train eighthTrain = new Train(numCars, 6, 1);
-//        trainQ.add(eighthTrain);
-//        TrainEvent eighthTrainEvent = new TrainEvent(eighthTrain);
-//        eventQ.add(eighthTrainEvent);
-//
-//        Train ninthTrain = new Train(numCars, 16, -1);
-//        trainQ.add(ninthTrain);
-//        TrainEvent ninthTrainEvent = new TrainEvent(ninthTrain);
-//        eventQ.add(ninthTrainEvent);
-//
-//        Train tenthTrain = new Train(numCars, 16, 1);
-//        trainQ.add(tenthTrain);
-//        TrainEvent tenthTrainEvent = new TrainEvent(tenthTrain);
-//        eventQ.add(tenthTrainEvent);
-//
-//        Train eleventhTrain = new Train(numCars, 4, -1);
-//        trainQ.add(eleventhTrain);
-//        TrainEvent eleventhTrainEvent = new TrainEvent(eleventhTrain);
-//        eventQ.add(eleventhTrainEvent);
-//
-//        Train twelthTrain = new Train(numCars, 4, 1);
-//        trainQ.add(twelthTrain);
-//        TrainEvent twelthTrainEvent = new TrainEvent(twelthTrain);
-//        eventQ.add(twelthTrainEvent);
-//
-//        Train thirteenTrain = new Train(numCars, 19, -1);
-//        trainQ.add(thirteenTrain);
-//        TrainEvent thirteenTrainEvent = new TrainEvent(thirteenTrain);
-//        eventQ.add(thirteenTrainEvent);
-//
-//        Train fourteenTrain = new Train(numCars, 19, 1);
-//        trainQ.add(fourteenTrain);
-//        TrainEvent fourteenTrainEvent = new TrainEvent(fourteenTrain);
-//        eventQ.add(fourteenTrainEvent);
-//
-//        Train fifteenTrain = new Train(numCars, 9, -1);
-//        trainQ.add(fifteenTrain);
-//        TrainEvent fifteenTrainEvent = new TrainEvent(fifteenTrain);
-//        eventQ.add(fifteenTrainEvent);
-//
-//        Train sixteenTrain = new Train(numCars, 9, 1);
-//        trainQ.add(sixteenTrain);
-//        TrainEvent sixteenTrainEvent = new TrainEvent(sixteenTrain);
-//        eventQ.add(sixteenTrainEvent);
-//
-//        Train seventeenTrain = new Train(numCars, 14, -1);
-//        trainQ.add(seventeenTrain);
-//        TrainEvent seventeenTrainEvent = new TrainEvent(seventeenTrain);
-//        eventQ.add(seventeenTrainEvent);
-//
-//        Train eighteenTrain = new Train(numCars, 14, 1);
-//        trainQ.add(eighteenTrain);
-//        TrainEvent eighteenTrainEvent = new TrainEvent(eighteenTrain);
-//        eventQ.add(eighteenTrainEvent);
-//
-//        Train nineteenTrain = new Train(numCars, 2, -1);
-//        trainQ.add(nineteenTrain);
-//        TrainEvent nineteenTrainEvent = new TrainEvent(nineteenTrain);
-//        eventQ.add(nineteenTrainEvent);
-//
-//        Train twentyTrain = new Train(numCars, 2, 1);
-//        trainQ.add(twentyTrain);
-//        TrainEvent twentyTrainEvent = new TrainEvent(twentyTrain);
-//        eventQ.add(twentyTrainEvent);
-//
-//        Train twentyoneTrain = new Train(numCars, 22, -1);
-//        trainQ.add(twentyoneTrain);
-//        TrainEvent twentyoneTrainEvent = new TrainEvent(twentyoneTrain);
-//        eventQ.add(twentyoneTrainEvent);
-//
-//        Train twentytwoTrain = new Train(numCars, 22, -1);
-//        trainQ.add(twentytwoTrain);
-//        TrainEvent twentytwoTrainEvent = new TrainEvent(twentytwoTrain);
-//        eventQ.add(twentytwoTrainEvent);
-//
-//        Train twentythreeTrain = new Train(numCars, 13, -1);
-//        trainQ.add(twentythreeTrain);
-//        TrainEvent twentythreeTrainEvent = new TrainEvent(twentythreeTrain);
-//        eventQ.add(twentythreeTrainEvent);
-//
-//        int i = 0;
-//        while (i < numTrains){
-//            Train added = (Train) trainQ.remove();
-//            TrainArray.add();
-//            agenda.add(((TrainEvent) eventQ.remove()), 0);
-//        }
-//
-//    }
 
     public static void main(String[] args) {
         Stop.stopSetUp();
@@ -213,6 +88,8 @@ public class GreenlineSim {
         agenda.add(centralS, 0);
         DowntownPassengerMaker unionD = new DowntownPassengerMaker(23);
         agenda.add(unionD, 0);
+        LineLengthEvent lenData = new LineLengthEvent();
+        agenda.add(lenData, 0);
 
 
         while (GreenlineSim.agenda.getCurrentTime() < 1440){
@@ -221,11 +98,12 @@ public class GreenlineSim {
             removed.run();
         }
 
-        GreenlineSim.write("dataFile");
+        GreenlineSim.writeFileOne("dataFile");
+        GreenlineSim.writeFileTwo("dataFile2");
 
     }
 
-    public static boolean write(String fileName) {
+    public static boolean writeFileOne(String fileName) {
         PrintWriter p = null;
         try {
             p = new PrintWriter(new File(fileName));
@@ -240,6 +118,25 @@ public class GreenlineSim {
             p.println(numTrainsR + " " + numCarsR + ", " + i + ", " + t.getArrived() + ", " + t.getArrivaltime() + ", "
                     + t.getDestinationtime() + ", " + t.getToaltriptime() + ", " + t.getArrivalstop() + ", "
                     + t.getDestinationstop() + ", " + t.getDirectionvector());
+            i++;
+        }
+        p.close();
+        return true;
+    }
+
+    public static boolean writeFileTwo(String fileName) {
+        PrintWriter p = null;
+        try {
+            p = new PrintWriter(new File(fileName));
+        } catch (Exception e) {
+            return false;
+        }
+        p.println("Stop#, East, West");
+        int i = 1;
+        while (passengerArray.length() != 0) {
+            int east = (Integer) lineLengthData.remove();
+            int west = (Integer) lineLengthData.remove();
+            p.println(i + ", " + east + ", " + west);
             i++;
         }
         p.close();
